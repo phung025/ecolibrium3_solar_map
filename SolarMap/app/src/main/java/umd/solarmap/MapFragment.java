b6065b37c79ac15f86596c5dceffb345580c2241
Cody Seavey <seave037@d.umn.edu>
2016-10-28 00:06:58 -0500

    added into insol_dlh_annovtpk... also using beta api otherwise the vectortiledlayers cannot be used

--- SolarMap/app/src/main/java/umd/solarmap/MapFragment.java
+++ SolarMap/app/src/main/java/umd/solarmap/MapFragment.java
@@ -1,174 +1,103 @@
 package umd.solarmap;
 
-import android.Manifest;
 import android.content.Context;
-import android.content.pm.PackageManager;
-import android.graphics.Color;
-import android.location.Location;
-import android.location.LocationListener;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
-import android.support.v4.content.ContextCompat;
-import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.esri.android.map.GraphicsLayer;
-import com.esri.android.map.LocationDisplayManager;
-import com.esri.android.map.MapView;
-import com.esri.android.map.ags.ArcGISImageServiceLayer;
-import com.esri.android.map.event.OnStatusChangedListener;
-import com.esri.core.geometry.Envelope;
-import com.esri.core.geometry.Point;
-import com.esri.core.map.Graphic;
-import com.esri.core.symbol.PictureMarkerSymbol;
-import com.esri.core.symbol.TextSymbol;
-import com.esri.core.tasks.geocode.Locator;
-import com.esri.core.tasks.geocode.LocatorFindParameters;
-import com.esri.core.tasks.geocode.LocatorGeocodeResult;
+import com.esri.arcgisruntime.concurrent.ListenableFuture;
+import com.esri.arcgisruntime.geometry.Point;
+import com.esri.arcgisruntime.layers.ArcGISVectorTiledLayer;
+import com.esri.arcgisruntime.mapping.ArcGISMap;
+import com.esri.arcgisruntime.mapping.Basemap;
+import com.esri.arcgisruntime.mapping.Viewpoint;
+import com.esri.arcgisruntime.mapping.view.LocationDisplay;
+import com.esri.arcgisruntime.mapping.view.MapView;
+import com.esri.arcgisruntime.tasks.geocode.GeocodeParameters;
+import com.esri.arcgisruntime.tasks.geocode.GeocodeResult;
+import com.esri.arcgisruntime.tasks.geocode.LocatorTask;
 
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+
 
 /**
  * To create a fragment, extend the Fragment class, then override key lifecycle methods to insert your app logic, similar to the way you would with an Activity class.
  * One difference when creating a Fragment is that you must use the onCreateView() callback to define the layout. In fact, this is the only callback you need in order
  * to get a fragment running.
  */
-
 public class MapFragment extends Fragment {
 
-    //region Instance Field
     public static final int DEFAULT_MAP_ZOOM = 7;
 
-    // UI Components
     private MapView mainMapView;
+
     private EditText searchTextField;
     private FloatingActionButton searchButton;
     private FloatingActionButton toCurrentLocationButton;
+    final LocatorTask locatorTask = new LocatorTask("http://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer");
+    private GeocodeParameters geocodeParams;
 
-    // arcGIS Location Display Manager
-    LocationDisplayManager mapLocationDisplayManager;
 
-    // Arguments for the location retrieved from searching
-    private GraphicsLayer queryLayer;
-    private Point locationLayerPoint;
-    private String locationLayerPointString;
-
-    //endregion
-
-    /**
-     * This function is automatically called to inflate the fragment from an activity
-     * @param inflater
-     * @param container
-     * @param savedInstanceState
-     * @return fragment view
-     */
-    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_map, container, false);
     }
 
     public void onActivityCreated(Bundle savedInstance) {
         super.onActivityCreated(savedInstance);
 
-        // Get the map view.
-        // NOTE: This only works after onCreateView() (i.e onActivityCreated())
         mainMapView = (MapView) getActivity().findViewById(R.id.mainMapView);
-        queryLayer = new GraphicsLayer();
-        mainMapView.addLayer(queryLayer);
+        Basemap basemap = Basemap.createImagery();
+        ArcGISMap map = new ArcGISMap(basemap);
+        Viewpoint vp = new Viewpoint(47.606726, -122.335564, 72223.819286);
+        map.setInitialViewpoint(vp);
+        mainMapView.setMap(map);
 
-        mainMapView.addLayer(new ArcGISImageServiceLayer(
-                "https://gis.uspatial.umn.edu/arcgis/rest/services/solar/mn_solar/ImageServer",null));
+        ArcGISVectorTiledLayer layer1 = new ArcGISVectorTiledLayer("http://tiles.arcgis.com/tiles/8df8p0NlLFEShl0r/arcgis/rest/services/insol_dlh_annovtpk/VectorTileServer");
+        map.getOperationalLayers().add(layer1);
 
-        // Get the search text field & search button
+
         searchTextField = (EditText) getActivity().findViewById(R.id.locationSearchTextField);
         searchButton = (FloatingActionButton) getActivity().findViewById(R.id.locationSearchActionButton);
         toCurrentLocationButton = (FloatingActionButton) getActivity().findViewById(R.id.toCurrentLocationButton);
+        geocodeParams = new GeocodeParameters();
+        geocodeParams.setCountryCode("United States");
 
-        this.setupMap();
-        this.setupTextField();
-        this.setupButtons();
+        setupMap();
+        setupTextField();
+        setupButtons();
     }
 
-
-    //region Setup the UI Components Action, etc
-
-    /**
-     * Set up all actions associated with the map view
-     */
     private void setupMap() {
-
         // Set a listener that will be called when the MapView is initialized.
         // Check if the map is ready for a user to make queries.
         //region mainMapView.OnStatusChangedListener()
-        mainMapView.setOnStatusChangedListener((source, status) -> {
 
-            if ((source == mainMapView) && (status == OnStatusChangedListener.STATUS.INITIALIZED)) {
-                // Enable the search text field only when the map is initialized
-                searchTextField.setEnabled(true);
+        mainMapView.addDrawStatusChangedListener(drawStatusChangedEvent -> {
+            // Enable the search text field only when the map is initialized
+            searchTextField.setEnabled(true);
 
-                // Get location display manager from arcGIS
-                mapLocationDisplayManager = mainMapView.getLocationDisplayManager();
-                mapLocationDisplayManager.setAutoPanMode(LocationDisplayManager.AutoPanMode.LOCATION);
-                mapLocationDisplayManager.setLocationListener(new LocationListener() {
-
-                        @Override
-                        public void onLocationChanged(Location location) {
-
-                            // Get current location of the device
-                            Point currentLocationPoint = new Point(location.getLatitude(), location.getLongitude());
-
-                            // Zoom the map to the current location
-                            //mainMapView.zoomTo(currentLocationPoint, MapFragment.DEFAULT_MAP_ZOOM);
-                        }
-
-                        @Override
-                        public void onStatusChanged(String s, int i, Bundle bundle) {
-                        }
-
-                        @Override
-                        public void onProviderEnabled(String s) {
-                        }
-
-                        @Override
-                        public void onProviderDisabled(String s) {
-                        }
-                    });
-                mapLocationDisplayManager.setShowLocation(true);
-                mapLocationDisplayManager.start();
-            }
+            mainMapView.getLocationDisplay().setAutoPanMode(LocationDisplay.AutoPanMode.OFF); //changed from LocationDisplayManager.AutoPanMode.LOCATION
+            mainMapView.getLocationDisplay().addLocationChangedListener(locationChangedEvent -> {
+                Point currentLocationPoint = locationChangedEvent.getSource().getLocation().getPosition();
+            });
+            mainMapView.getLocationDisplay().setShowLocation(true);
+            mainMapView.getLocationDisplay().startAsync();
         });
-        //endregion
-
-        // Set a listener that will be called when user press a point on
-        // the map for too long.
-        // This listener is implemented as an action when user touch a position
-        // on the map for long time.
-        // If that position corresponds to a marker, display options for that marker. (i.e remove marker)
-        // Otherwise, display options such as mark location for interest or add to saved location
-        //region mainMapView.setOnLongPressListener()
-        mainMapView.setOnLongPressListener((x_coordinate, y_coordinate) -> {
-
+        mainMapView.setOnLongClickListener(view -> {
             Toast.makeText(getContext(), "What took you so long?", Toast.LENGTH_LONG).show();
             // Return true that the listener has consumed the event
             return true;
         });
-        //endregion
     }
 
-    /**
-     * Set up all actions associated with the buttons in this fragment
-     */
     private void setupButtons() {
 
         /**
@@ -182,10 +111,27 @@ public class MapFragment extends Fragment {
             inputManager.hideSoftInputFromWindow(getActivity().getCurrentFocus().getWindowToken(), 0);
 
             String address = searchTextField.getText().toString();
-            if (!address.equals(""))
-                executeLocatorTask(address);
+            if (!address.equals("")){
+                final ListenableFuture<List<GeocodeResult>> geocodeFuture = locatorTask.geocodeAsync(address, geocodeParams);
+
+                geocodeFuture.addDoneListener(() -> {
+                    try {
+                        List<GeocodeResult> geocodeResults = geocodeFuture.get();
+
+                        // Use the first result - for example display on the map
+                        GeocodeResult topResult = geocodeResults.get(0);
+                        mainMapView.setViewpointCenterAsync(topResult.getDisplayLocation());
+                    }
+                    catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    catch (ExecutionException e) {
+                        e.printStackTrace();
+                    }
+                });
+            }
+
         });
-        //endregion
 
         /**
          * Action when clicking go to current location button
@@ -193,18 +139,16 @@ public class MapFragment extends Fragment {
         //region onClickedToCurrentLocationButton()
         toCurrentLocationButton.setOnClickListener(view -> {
 
-            Point currentLocationPoint = mapLocationDisplayManager.getPoint();
+            Point currentLocationPoint = mainMapView.getLocationDisplay().getLocation().getPosition();
 
             // If the current location is detected
             if (currentLocationPoint != null) {
-
                 // Zoom the map to current location
-                mainMapView.zoomTo(mapLocationDisplayManager.getPoint(), MapFragment.DEFAULT_MAP_ZOOM);
+                mainMapView.setViewpointCenterAsync(mainMapView.getLocationDisplay().getLocation().getPosition());
             } else {
                 Toast.makeText(getContext(), getString(R.string.cantLocateCurrentPosition), Toast.LENGTH_LONG).show();
             }
         });
-        //endregion
     }
 
     /**
@@ -221,98 +165,8 @@ public class MapFragment extends Fragment {
             return false;
         });
     }
-    //endregion
-
-    //region Functions & Background Thread for Executing Location Search
-    private void executeLocatorTask(String address) {
-
-        // Create Locator parameters from single line address string
-        LocatorFindParameters findParams = new LocatorFindParameters(address);
-
-        // Use the centre of the current map extent as the find location point
-        findParams.setLocation(mainMapView.getCenter(), mainMapView.getSpatialReference());
-
-        // Calculate distance for find operation
-        Envelope mapExtent = new Envelope();
-        mainMapView.getExtent().queryEnvelope(mapExtent);
-
-        // assume map is in metres, other units wont work, double current envelope
-        double distance = (mapExtent != null && mapExtent.getWidth() > 0) ? mapExtent.getWidth() * 2 : 10000;
-        findParams.setDistance(distance);
-        findParams.setMaxLocations(2);
-
-        // Set address spatial reference to match map
-        findParams.setOutSR(mainMapView.getSpatialReference());
-
-        // Execute async task to find the address
-        new LocatorAsyncTask().execute(findParams);
-        locationLayerPointString = address; // Why do we need this?
-    }
-    private class LocatorAsyncTask extends AsyncTask<LocatorFindParameters, Void, List<LocatorGeocodeResult>> {
-        private Exception mException;
-
-        @Override
-        protected List<LocatorGeocodeResult> doInBackground(LocatorFindParameters... params) {
-            mException = null;
-            List<LocatorGeocodeResult> results = null;
-            Locator locator = Locator.createOnlineLocator();
-            try {
-                results = locator.find(params[0]);
-            } catch (Exception e) {
-                mException = e;
-            }
-            return results;
-        }
-
-        protected void onPostExecute(List<LocatorGeocodeResult> result) {
-            if (mException != null) {
-                Log.w("PlaceSearch", "LocatorSyncTask failed with:");
-                mException.printStackTrace();
-                Toast.makeText(getContext(), getString(R.string.addressSearchFailed), Toast.LENGTH_LONG).show();
-                return;
-            }
-
-            if (result.size() == 0) {
-                Toast.makeText(getContext(), getString(R.string.noResultsFound), Toast.LENGTH_LONG).show();
-            } else {
-
-                // Use first result in the list
-                LocatorGeocodeResult geocodeResult = result.get(0);
-
-                // get return geometry from geocode result
-                Point resultPoint = geocodeResult.getLocation();
-
-                // create marker symbol to represent location
-                PictureMarkerSymbol resultSymbol = new PictureMarkerSymbol(getContext(), ContextCompat.getDrawable(getContext(), R.drawable.query_location_marker));
-                //SimpleMarkerSymbol resultSymbol = new SimpleMarkerSymbol(Color.RED, 16, SimpleMarkerSymbol.STYLE.CROSS);
-
-                // create graphic object for resulting location
-                Graphic resultLocGraphic = new Graphic(resultPoint, resultSymbol);
-
-                // Remove all previous location search markers in the query layer
-                queryLayer.removeAll();
-
-                // add graphic to location layer
-                queryLayer.addGraphic(resultLocGraphic);
-
-                // create text symbol for return address
-                String address = geocodeResult.getAddress();
-                TextSymbol resultAddress = new TextSymbol(20, address, Color.BLACK);
-                // create offset for text
-                resultAddress.setOffsetX(-4 * address.length());
-                resultAddress.setOffsetY(10);
-                // create a graphic object for address text
-                Graphic resultText = new Graphic(resultPoint, resultAddress);
-
-                // add address text graphic to location graphics layer
-                queryLayer.addGraphic(resultText);
-
-                locationLayerPoint = resultPoint;
-
-                // Zoom map to geocode result location
-                mainMapView.zoomToResolution(geocodeResult.getLocation(), MapFragment.DEFAULT_MAP_ZOOM);
-            }
-        }
-    }
-    //endregion
 }
+
+
+
+
