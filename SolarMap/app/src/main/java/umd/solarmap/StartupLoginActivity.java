85fb40c54623a3c4ed71ae2af1f6b682ee637a4a
Nam Phung <phungle.thanhnam@gmail.com>
2016-11-13 18:44:14 -0600

    Added: sign up button touch action [Send POST request to server for signing up account]

--- SolarMap/app/src/main/java/umd/solarmap/StartupLoginActivity.java
+++ SolarMap/app/src/main/java/umd/solarmap/StartupLoginActivity.java
@@ -1,39 +1,23 @@
 package umd.solarmap;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.annotation.TargetApi;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.app.LoaderManager.LoaderCallbacks;
-
-import android.content.CursorLoader;
-import android.content.Loader;
-import android.database.Cursor;
-import android.net.Uri;
-import android.os.AsyncTask;
-
-import android.os.Build;
 import android.os.Bundle;
-import android.provider.ContactsContract;
-import android.text.TextUtils;
-import android.view.KeyEvent;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.inputmethod.EditorInfo;
-import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.mashape.unirest.http.Headers;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.async.Callback;
+import com.mashape.unirest.http.exceptions.UnirestException;
 
-import static android.Manifest.permission.READ_CONTACTS;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.InputStream;
+import java.util.concurrent.Future;
 
 /**
  * A login screen that offers login via email/password.
@@ -43,6 +27,8 @@ public class StartupLoginActivity extends AppCompatActivity {
     // UI references.
     private EditText mEmailView;
     private EditText mPasswordView;
+    private Button mSignInButton;
+    private Button mSignUpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,11 +37,38 @@ public class StartupLoginActivity extends AppCompatActivity {
 
         mEmailView = (EditText) findViewById(R.id.email_field);
         mPasswordView = (EditText) findViewById(R.id.password_field);
+        mSignInButton = (Button) findViewById(R.id.sign_in_button);
+        mSignUpButton = (Button) findViewById(R.id.sign_up_button);
 
     }
 
     public void onTouchSignInButton(View view) {
         finish();
+    }
+
+    public void onTouchSignUpButton(View view) {
+
+        // Both text fields must not be empty
+
+        try {
+            String email_address = mEmailView.getText().toString();
+            String password = mPasswordView.getText().toString();
+
+            JSONObject signUpInfo = new JSONObject();
+            signUpInfo.put("email", email_address);
+            signUpInfo.put("password", password);
+
+            HTTPAsyncTask jsonResponse ;
+            (jsonResponse = new HTTPAsyncTask() {
+                @Override
+                protected void onPostExecute(String result) {
+                    System.out.println(result);
+                }
+            }).execute("http://10.0.2.2:4321/registerAccount", "POST", signUpInfo.toString());
+
+        } catch (JSONException jsonException) {
+            jsonException.printStackTrace();
+        }
     }
 }
 
